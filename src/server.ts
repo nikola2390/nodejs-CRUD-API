66d150e69fd9619b94d5880736cbe87cc63752b5
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-10-19 18:47:54 +0300

    feat: create default server

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import http from "node:http";
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
+
+server.listen(PORT);
