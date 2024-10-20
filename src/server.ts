2f192d81658c33f29f106e1a80ec53c5d455f900
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-10-20 10:52:51 +0300

    feat: implement get all users with mocked data

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,62 @@ import { configDotenv } from "dotenv";
 
 configDotenv();
 
-const server = http.createServer((req, res) => {});
+const users: User[] = [
+  {
+    id: "544444",
+    username: "John",
+    age: 33,
+    hobbies: ["football"],
+  },
+  {
+    id: "54444",
+    username: "Johns",
+    age: 22,
+    hobbies: [],
+  },
+  {
+    id: "5444",
+    username: "Jo",
+    age: 34,
+    hobbies: ["one"],
+  },
+  {
+    id: "544",
+    username: "Joh",
+    age: 51,
+    hobbies: ["hobby1", "hobby2"],
+  },
+];
+
+interface User {
+  id: string;
+  username: string;
+  age: number;
+  hobbies: string[];
+}
+
+enum StatusCode {
+  OK = 200,
+}
+
+enum HttpMethod {
+  GET = "GET",
+}
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case HttpMethod.GET:
+      if (req.url === "/api/users") {
+        res.write(JSON.stringify(users));
+        res.statusCode = StatusCode.OK;
+      }
+      break;
+
+    default:
+      break;
+  }
+
+  res.end();
+});
 
 server.listen(process.env.PORT);
