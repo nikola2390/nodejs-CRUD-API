b8d4c91d9454b96a30da6dc0e23ad2ed002dedd3
Mikalai Shalupin <mikalai.shalupin@gmail.com>
2024-10-21 21:07:49 +0300

    feat: implement GET requests

--- src/constants.ts
+++ src/constants.ts
@@ -7,8 +7,14 @@ export interface User {
 
 export enum StatusCode {
   OK = 200,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
 }
 
 export enum HttpMethod {
   GET = "GET",
 }
+
+export const userNotExistsMessage = "The requested user does not exist";
+
+export const invalidUserIdMessage = "User ID is invalid";
--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,73 @@
 import http from "node:http";
 import { configDotenv } from "dotenv";
+import { v4 as uuidv4, validate as validateUuidv4 } from "uuid";
 
-import { User, StatusCode, HttpMethod } from "./constants";
+import {
+  User,
+  StatusCode,
+  HttpMethod,
+  userNotExistsMessage,
+  invalidUserIdMessage,
+} from "./constants";
 
 configDotenv();
 
 const users: User[] = [
   {
-    id: "544444",
+    id: uuidv4(),
     username: "John",
     age: 33,
     hobbies: ["football"],
   },
   {
-    id: "54444",
+    id: uuidv4(),
     username: "Johns",
     age: 22,
     hobbies: [],
   },
   {
-    id: "5444",
+    id: uuidv4(),
     username: "Jo",
     age: 34,
     hobbies: ["one"],
   },
   {
-    id: "544",
+    id: uuidv4(),
     username: "Joh",
     age: 51,
     hobbies: ["hobby1", "hobby2"],
   },
 ];
 
+const parseURL = (url: string): string[] => {
+  return url.split("/");
+};
+
 const server = http.createServer((req, res) => {
+  const userID: string | undefined = parseURL(req.url!)[3];
+
   switch (req.method) {
     case HttpMethod.GET:
       if (req.url === "/api/users") {
         res.write(JSON.stringify(users));
         res.statusCode = StatusCode.OK;
+      }
+
+      if (req.url!.startsWith("/api/users") && validateUuidv4(userID)) {
+        const user: User | undefined = users.find((user) => user.id === userID);
+
+        if (user) {
+          res.statusCode = StatusCode.OK;
+          res.write(JSON.stringify(user));
+        } else {
+          res.statusCode = StatusCode.NOT_FOUND;
+          res.write(JSON.stringify({ message: userNotExistsMessage }));
+        }
+      }
+
+      if (req.url!.startsWith("/api/users") && !validateUuidv4(userID)) {
+        res.statusCode = StatusCode.BAD_REQUEST;
+        res.write(JSON.stringify({ message: invalidUserIdMessage }));
       }
       break;
 
